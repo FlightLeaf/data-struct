cced3b5226246680c8c2d8910ca5e9b4f16d9aee
Flight_leaf <718856370@qq.com>
2023-05-08 22:43:18 +0800

    README.md修改

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -1,13 +1,14 @@
-#include<iostream>
-template<class T>
+#include <iostream>
+template <class T>
 class LinkNode
 {
-    public:
-        LinkNode *link;
-        T data;
-    public:
-    LinkNode(LinkNode *ptr = NULL){link = ptr;};
-    LinkNode(const T &item,LinkNode *ptr = NULL)
+public:
+    LinkNode *link;
+    T data;
+
+public:
+    LinkNode(LinkNode *ptr = NULL) { link = ptr; };
+    LinkNode(const T &item, LinkNode *ptr = NULL)
     {
         data = item;
         link = ptr;
