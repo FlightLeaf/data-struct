93153a35b1650fa67a956733b0bad805c12b3b1c
Flight_leaf <718856370@qq.com>
2023-05-11 23:00:59 +0800

    队列实现杨辉三角

--- src/StacksAndQueues/SeqQueueTest.cpp
+++ src/StacksAndQueues/SeqQueueTest.cpp
@@ -1,20 +1,10 @@
-#include"SeqQueue.cpp"
+#include "SeqQueue.cpp"
 
 using namespace std;
 
 int main()
 {
-    SeqQueue<int> queue(14);
-    
-    for(int i=0; i<10; i++)
-    {
-        queue.EnQueue(i);
-    }
-    int front;
-    queue.getFront(front);
-    cout << queue << endl;  
-    cout<< "********************************"<<endl;
-    cout << front << endl;
-    cout << queue.getSize() << endl;
+    SeqQueue<int> queue(20);
+    queue.YANGVI(10);
     return 0;
 }
