faf9e57f6bc8e689e1a9c9d9ea1d74e3427aabaa
Flight_leaf <718856370@qq.com>
2023-06-01 17:19:50 +0800

    二叉树

--- src/ArraysAndStrings/Trituple.cpp
+++ src/ArraysAndStrings/Trituple.cpp
@@ -42,19 +42,24 @@ public:
         Rows = Cols = Terms = 0;
     }
     /// @brief 析构函数
-    ~SparseMatrix() { delete[] smArrays; };
+    ~SparseMatrix() { //delete[] smArrays;
+     };
 
+    /// @brief 输入矩阵
     void input();
 
+    /// @brief 输出矩阵
     void output();
 
-    //void transpose(SparseMatrix &x);
+    /// @brief 转置
+    /// @param x 转置后的矩阵
+    void transpose(SparseMatrix &x);
 
-    /// @brief 矩阵求和 x = x + y 
-    /// @param x 
+    /// @brief 矩阵求和 x = x + y
+    /// @param x
     /// @param y
     void add(SparseMatrix &x,SparseMatrix &y);
-    
+
 private:
     /// @brief 行数
     int Rows;
@@ -74,32 +79,124 @@ private:
 
 void SparseMatrix::input()
 {
-    cin>>Rows>>Cols>>Terms;
-    for(int i=0; i<Terms;i++)
+    cin >> Rows >> Cols >> Terms;
+    for (int i = 0; i < Terms; i++)
     {
-        cin>>smArrays[i].row>>smArrays[i].col>>smArrays[i].value;
+        cin >> smArrays[i].row >> smArrays[i].col >> smArrays[i].value;
     }
 }
 
 void SparseMatrix::output()
 {
-    cout<<Rows<<" "<<Cols<<" "<<Terms<<endl;
-    for(int i=0;i<Terms;i++)
+    for (int i = 0; i < Terms; i++)
     {
-        cout<<smArrays[i].row<<" "<<smArrays[i].col<<" "<<smArrays[i].value<<endl;
+        cout << smArrays[i].row << " " << smArrays[i].col << " " << smArrays[i].value << endl;
     }
 }
 
-void SparseMatrix::add(SparseMatrix& x,SparseMatrix& y)
+void SparseMatrix::add(SparseMatrix &x,SparseMatrix &y)
 {
-    
+    if (x.Rows != y.Rows || x.Cols != y.Cols) 
+    {
+        cerr << "矩阵行列不匹配！" << endl;
+        return;
+    }
+    SparseMatrix z(x.maxTerms + y.maxTerms);
 
+    int p1 = 0, p2 = 0, p3 = 0;
+
+    while (p1 < x.Terms && p2 < y.Terms)
+    {
+        if (x.smArrays[p1].row < y.smArrays[p2].row ||
+            (x.smArrays[p1].row == y.smArrays[p2].row &&
+             x.smArrays[p1].col < y.smArrays[p2].col))
+        {
+            z.smArrays[p3++] = x.smArrays[p1++];
+        }
+        else if (x.smArrays[p1].row > y.smArrays[p2].row ||
+                 (x.smArrays[p1].row == y.smArrays[p2].row &&
+                  x.smArrays[p1].col > y.smArrays[p2].col))
+        {
+            z.smArrays[p3++] = y.smArrays[p2++];
+        }
+        else
+        {
+            Trituple temp;
+            temp.row = x.smArrays[p1].row;
+            temp.col = x.smArrays[p1].col;
+            temp.value = x.smArrays[p1].value + y.smArrays[p2].value;
+            if (temp.value != 0) 
+            {
+                z.smArrays[p3++] = temp;
+            }
+            p1++;
+            p2++;
+        }
+    }
+
+    while (p1 < x.Terms)
+    {
+        z.smArrays[p3++] = x.smArrays[p1++];
+    }
+    while (p2 < y.Terms)
+    {
+        z.smArrays[p3++] = y.smArrays[p2++];
+    }
+
+    z.Rows = x.Rows;
+    z.Cols = x.Cols;
+    z.Terms = p3;
+
+    x = z;
+}
+
+ 
+void SparseMatrix::transpose(SparseMatrix &x)
+{
+    int *rowSize = new int[Cols];
+    int *rowStart = new int[Cols];
+    x.Cols = Rows;
+    x.Rows = Cols;
+    x.Terms = Terms;
+    if (Terms > 0)
+    {
+        int i, j;
+        for (i = 0; i < Cols; i++)
+        {
+            rowSize[i] = 0;
+        }
+        for (i = 0; i < Terms; i++)
+        {
+            rowSize[smArrays[i].col]++;
+        }
+        rowStart[0] = 0;
+        for (i = 1; i < Cols; i++)
+        {
+            rowStart[i] = rowStart[i - 1] + rowSize[i - 1];
+        }
+        for(i = 0; i < Terms; i++)
+        {
+            j = rowStart[smArrays[i].col];
+            x.smArrays[j].row = smArrays[i].col;
+            x.smArrays[j].col = smArrays[i].row;
+            x.smArrays[j].value = smArrays[i].value;
+            rowStart[smArrays[i].col] ++;
+        }
+        delete[] rowStart;
+        delete[] rowSize;
+    }
 }
 
 int main()
 {
-    SparseMatrix matrix;
-    matrix.input();
-    matrix.output();
+    SparseMatrix matrix1,matrix2,transform;
+    matrix1.input();
+    matrix2.input();
+    matrix1.transpose(transform);
+    cout<<"The transformed matrix  is:"<<endl;
+    transform.output();
+    matrix1.add(matrix1,matrix2);
+    cout<<"The added matrix is:"<<endl;
+    matrix1.output();
     return 0;
 }
---
+++ src/TreeAndBinaryTree/tree.cpp
@@ -0,0 +1,281 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+class BinaryTree;
+class BinTreeNode
+{ // 结点类的定义
+    friend BinaryTree;
+
+public:
+    BinTreeNode()
+    {
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+    BinTreeNode(char x, BinTreeNode *left = NULL, BinTreeNode *right = NULL) : data(x), leftChild(left), rightChild(right) {} // 构造函数
+    ~BinTreeNode() {}                                                                                                         // 析构函数
+private:
+    BinTreeNode *leftChild, *rightChild; // 左、右子女链域
+    char data;                           // 数据域
+};
+class BinaryTree
+{
+public:
+    BinaryTree() : root(NULL){};
+    BinaryTree(char value)
+    {
+        RefValue = value;
+        root = NULL;
+    }
+    ~BinaryTree() { destroy(root); }
+    /// @brief 创建书
+    void CreateBinTree() { CreateBinTree(root); }
+
+    /// @brief 是否为空
+    /// @return true 空
+    bool IsEmpty() { return (root == NULL) ? true : false; }
+
+    /// @brief 获取双亲节点
+    /// @param current 带获取对象
+    /// @return 双亲结点指针
+    BinTreeNode *Parent(BinTreeNode *current)
+    {
+        return (root == NULL || root == current) ? NULL : Parent(root, current);
+    }
+
+    /// @brief 获取左孩子
+    /// @param current 带获取对象
+    /// @return 左孩子指针
+    BinTreeNode *LeftChild(BinTreeNode *current)
+    {
+        return (current != NULL) ? current->leftChild : NULL;
+    }
+
+    /// @brief 获取右孩子
+    /// @param current 带获取对象
+    /// @return 右孩子指针
+    BinTreeNode *RightChild(BinTreeNode *current)
+    {
+        return (current != NULL) ? current->rightChild : NULL;
+    }
+    /// @brief 计算树高度
+    /// @return 高度
+    int Height() { return Height(root); }
+
+    /// @brief 计算树的大小
+    /// @return 树的大小
+    int Size() { return Size(root); }
+
+    /// @brief 叶子节点计算
+    /// @return 叶子节点个数
+    int countLeaves() { return countLeaves(root); }
+
+    /// @brief 获取根节点
+    /// @return 根节点指针
+    BinTreeNode *GetRoot() const { return root; }
+
+    /// @brief 前序遍历
+    void preOrder() { preOrder(root); }
+
+    /// @brief 中序遍历
+    void inOrder() { inOrder(root); }
+
+    /// @brief 后序遍历
+    void postOrder() { postOrder(root); }
+
+    /// @brief 查找目标在树中的个数
+    /// @param target 待查找的目标
+    /// @return 个数
+    int countFind(char &target) { return countFind(root, target); };
+
+    void outPrint() { outPrint(root); };
+
+private:
+    BinTreeNode *root; // 二叉树的根指针
+    char RefValue;     // 数据输入停止标志
+
+    void CreateBinTree(BinTreeNode *&subTree);
+    BinTreeNode *Parent(BinTreeNode *subTree, BinTreeNode *current);
+    int Height(BinTreeNode *subTree);
+    int Size(BinTreeNode *subTree);
+    int countLeaves(BinTreeNode *subTree);
+    int countFind(BinTreeNode *subTree, char &target);
+    void outPrint(BinTreeNode *subTree, int indent = 0);
+    void preOrder(BinTreeNode *subTree);  // 前序遍历
+    void inOrder(BinTreeNode *subTree);   // 中序遍历
+    void postOrder(BinTreeNode *subTree); // 后序遍历
+    void destroy(BinTreeNode *&subTree){};
+};
+
+void BinaryTree ::CreateBinTree(BinTreeNode *&subTree)
+{ // 私有函数: 建立根为subTree的子树
+    char item;
+    cin >> item;
+    if (item != RefValue)
+    {
+        subTree = new BinTreeNode(item);
+        CreateBinTree(subTree->leftChild);
+        CreateBinTree(subTree->rightChild);
+    }
+    else
+        subTree = NULL;
+};
+
+void BinaryTree ::inOrder(BinTreeNode *subTree)
+{
+    if (subTree != NULL)
+    {
+        inOrder(subTree->leftChild);
+        cout << subTree->data << " ";
+        inOrder(subTree->rightChild);
+    }
+}
+void BinaryTree ::preOrder(BinTreeNode *subTree)
+{
+    if (subTree != NULL)
+    {
+        cout << subTree->data << " ";
+        preOrder(subTree->leftChild);
+        preOrder(subTree->rightChild);
+    }
+}
+void BinaryTree ::postOrder(BinTreeNode *subTree)
+{
+    if (subTree != NULL)
+    {
+        postOrder(subTree->leftChild);
+        postOrder(subTree->rightChild);
+        cout << subTree->data << " ";
+    }
+}
+
+BinTreeNode *BinaryTree::Parent(BinTreeNode *subTree, BinTreeNode *current)
+{
+    if (subTree == NULL)
+        return NULL;
+    if (subTree->leftChild == current || subTree->rightChild == current)
+        return subTree;
+    BinTreeNode *temp;
+    if ((temp = Parent(subTree->leftChild, current)) != NULL)
+        return temp;
+    else
+        return Parent(subTree->rightChild, current);
+}
+
+int BinaryTree::Height(BinTreeNode *subTree)
+{
+    if (subTree == NULL)
+        return 0;
+    else
+    {
+        int i = Height(subTree->leftChild);
+        int j = Height(subTree->rightChild);
+        return (i < j ? j + 1 : i + 1);
+    }
+}
+
+int BinaryTree::Size(BinTreeNode *subTree)
+{
+    if (subTree == NULL)
+        return 0;
+    else
+        return 1 + Size(subTree->leftChild) + Size(subTree->rightChild);
+}
+
+int BinaryTree::countFind(BinTreeNode *subTree, char &target)
+{
+    int count = 0;
+    if (subTree == NULL)
+        return count;
+    if (target == subTree->data)
+        count++;
+    count += countFind(subTree->leftChild, target);
+    count += countFind(subTree->rightChild, target);
+    return count;
+}
+
+int BinaryTree::countLeaves(BinTreeNode *subTree)
+{
+    if (subTree == NULL)
+    { // 如果节点为空，返回0
+        return 0;
+    }
+    else if (!subTree->leftChild && !subTree->rightChild)
+    { // 如果节点是叶子节点，返回1
+        return 1;
+    }
+    else
+    { // 否则递归计算左右子树的叶子节点数
+        return countLeaves(subTree->leftChild) + countLeaves(subTree->rightChild);
+    }
+}
+
+void BinaryTree::outPrint(BinTreeNode *root, int indent)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << setw(indent)<< root->data << endl;
+    cout<<" ";
+    outPrint(root->leftChild, indent + 2);
+    outPrint(root->rightChild, indent + 2);
+}
+
+int main()
+{
+    char option;
+    BinaryTree a = BinaryTree('#');
+    while (cin >> option)
+    {
+        if (option == 'C')
+        {
+            a.CreateBinTree();
+            cout << "Created success!" << endl;
+        }
+        else if (option == 'H')
+        {
+            cout << "Height=" << a.Height() << "." << endl;
+        }
+        else if (option == 'L')
+        {
+            cout << "Leaf=" << a.countLeaves() << "." << endl;
+        }
+        else if (option == 'N')
+        {
+            cout << "Nodes=" << a.Size() << "." << endl;
+        }
+        else if (option == '1')
+        {
+            cout << "Preorder is:";
+            a.preOrder();
+            cout << "." << endl;
+        }
+        else if (option == '2')
+        {
+            cout << "Inorder is:";
+            a.inOrder();
+            cout << "." << endl;
+        }
+        else if (option == '3')
+        {
+            cout << "Postorder is:";
+            a.postOrder();
+            cout << "." << endl;
+        }
+        else if (option == 'F')
+        {
+            char item;
+            cin >> item;
+            cout << "The count of " << item << " is " << a.countFind(item) << "." << endl;
+        }
+        else if (option == 'P')
+        {
+            cout <<"The tree is:"<<endl;
+            a.outPrint();
+        }
+    }
+    return 0;
+}
